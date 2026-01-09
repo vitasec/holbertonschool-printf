08536d36f3967c67cba0930b372b13e41fd673d7
vitasec <davidabdurahmanov38@gmail.com>
2026-01-09 06:34:02 +0000

    task 2 done

--- _printf.c
+++ _printf.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * print_number - ədədləri simvol kimi çap edən köməkçi funksiya
+ * @n: çap ediləcək ədəd
+ * Return: çap edilən simvolların sayı
+ */
+int print_number(int n)
+{
+	unsigned int n1;
+	int count = 0;
+
+	if (n < 0)
+	{
+		count += _putchar('-');
+		n1 = -n;
+	}
+	else
+		n1 = n;
+
+	if (n1 / 10)
+		count += print_number(n1 / 10);
+
+	count += _putchar((n1 % 10) + '0');
+	return (count);
+}
+
+/**
  * _printf - produces output according to a format
  * @format: character string containing the directives
  *
